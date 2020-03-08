93ea58a94823a840ec17cea309112ef1f935d8e6
Ashish kumar <ashish735194@gmail.com>
2020-03-08 10:52:38 +0530

    pushing via vscode

---
+++ javascript/speech_recognition/speech-input.js
@@ -0,0 +1,28 @@
+function run(type, next){
+
+  next = next || 0
+    window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+    let finalTranscript = '';
+    let recognition = new window.SpeechRecognition();
+
+    recognition.interimResults = true;
+    recognition.maxAlternatives = 10;
+    recognition.continuous = true;
+
+    recognition.onresult = (event) => {
+      let interimTranscript = '';
+      for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
+        let transcript = event.results[i][0].transcript;
+        if (event.results[i].isFinal) {
+          finalTranscript += transcript;
+        } else {
+          interimTranscript += transcript;
+        }
+      }
+
+      document.getElementById(type).value = finalTranscript +  interimTranscript ;
+      
+    }
+    recognition.start();
+    //document.getElementById("handleinput").value="Hero"
+}
